2b97da0ec47babeeadf920c0c1a0ec37f5832b7c
mraakash09 <akash.nigam@legittai.com>
2024-07-17 23:34:29 +0530

    updated changes

--- src/pages/OnboardingDashboard/index.jsx
+++ src/pages/OnboardingDashboard/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-// import SidebarMenu from "../../components/SidebarMenu";
+import SidebarMenu from "../../components/SidebarMenu";
 
 export default function OnboardingDashboard() {
-  return <div>{/* <SidebarMenu /> */}</div>;
+  return (
+    <div>
+      <SidebarMenu />{" "}
+    </div>
+  );
 }
